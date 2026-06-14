0ab5bcf31242927aa7b1f8e91673ab4b8a816c31
gourisd007 <gourisd007@users.noreply.github.com>
2026-06-14 19:22:43 +0530

    Fix Vercel deployment

--- Backend/db.js
+++ Backend/db.js
@@ -8,7 +8,10 @@ const mongoUri =
   "mongodb://gourisd007_db_user:qJjOmFYCZbJuqrOw@ac-xt2utck-shard-00-00.c9kuz1j.mongodb.net:27017,ac-xt2utck-shard-00-01.c9kuz1j.mongodb.net:27017,ac-xt2utck-shard-00-02.c9kuz1j.mongodb.net:27017/?ssl=true&replicaSet=atlas-ax7h7q-shard-0&authSource=admin&appName=Cluster0";
 
 export const dbReady = mongoose
-  .connect(mongoUri)
+  .connect(mongoUri, {
+    serverSelectionTimeoutMS: 3000,
+    connectTimeoutMS: 3000,
+  })
   .then(() => {
     console.log("Db connected");
   })
